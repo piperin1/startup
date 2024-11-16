d522ea48b5d37302f442f5a410cc719cd979fd60
unknown <piperin@byu.edu>
2024-11-16 15:01:08 -0700

    backend adjustments and fixes

--- startupVite/service/index.js
+++ startupVite/service/index.js
@@ -1,10 +1,7 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
 import fetch from 'node-fetch';
 
 const app = express();
-
-let users = {};
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
@@ -13,40 +10,6 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// CreateAuth a new user
-apiRouter.post('/auth/create', async (req, res) => {
-  const user = users[req.body.email];
-  if (user) {
-    res.status(409).send({ msg: 'Existing user' });
-  } else {
-    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-    users[user.email] = user;
-
-    res.send({ token: user.token });
-  }
-});
-
-// GetAuth login an existing user
-apiRouter.post('/auth/login', async (req, res) => {
-  const user = users[req.body.email];
-  if (user) {
-    if (req.body.password === user.password) {
-      user.token = uuid.v4();
-      res.send({ token: user.token });
-      return;
-    }
-  }
-  res.status(401).send({ msg: 'Unauthorized' });
-});
-
-// DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
-  const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user) {
-    delete user.token;
-  }
-  res.status(204).end();
-});
 
 // Weather endpoint
 app.get('/api/weather', async (req, res) => {
