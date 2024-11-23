0c21bdba7c5fae0cde6e47d9059346016fea53c9
unknown <piperin@byu.edu>
2024-11-22 20:35:40 -0700

    fully debugged login without features

--- startupVite/src/login/login.jsx
+++ startupVite/src/login/login.jsx
@@ -7,7 +7,7 @@ export function Login({ userName, authState, onAuthChange }) {
   return (
     <section className="column">
       <div>
-        {authState !== AuthState.Unknown && <h4>Login</h4>}
+        {authState !== AuthState.Unknown && <h1 className="center">Login</h1>}
         {authState === AuthState.Authenticated && (
           <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
         )}
--- startupVite/src/main.jsx
+++ startupVite/src/main.jsx
@@ -31,7 +31,7 @@ function Main () {
                           }}
                         />
                       }
-            exact/>
+                    exact/>
                     <Route path="/home" element={<Simulator/>} />
                 </Routes>
           <footer>
