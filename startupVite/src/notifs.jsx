944f649c04d0d82b2a37e9f8cea5e4d255e70ce2
unknown <piperin@byu.edu>
2024-12-07 13:48:49 -0700

    mock notif code removed

--- startupVite/src/notifs.jsx
+++ startupVite/src/notifs.jsx
@@ -1,27 +1,3 @@
-/*import { useState, useEffect } from 'react';
-
-function MockNotification() {
-    const actions = ['fed', 'petted'];
-    const [notification, setNotification] = useState(`Someone has ${actions[0]} their pet`);
-    const [index, setIndex] = useState(0);
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setIndex((prevIndex) => (prevIndex + 1) % actions.length);
-      }, 3000);
-      
-      return () => clearInterval(interval);
-    }, []);
-  
-    useEffect(() => {
-      setNotification(`Someone has ${actions[index]} their pet`);
-    }, [index]);
-  
-    return <p>{notification}</p>;
-  }
-
-  export default MockNotification;*/
-
   import React from 'react';
   import { GameEvent, GameNotifier } from './gameNotifier';
   import './home.css';
